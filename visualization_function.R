6d271d0238b458ae99bc35284e9bcae487921078
iamqb0205 <iamqb@bu.edu>
2021-10-07 21:58:57 -0400

    10.7 Version2

--- visualization_function.R
+++ visualization_function.R
@@ -1,18 +1,30 @@
+#load the package tidyverse and ggplot2
 library(tidyverse)
 library(ggplot2)
 
+#Read R Code from the file "wrangling_code.R"
 source(file = "wrangling_code.R", echo = TRUE)
 
+#ggplot function to make the visualization, I set there arguments in this function.
+#The three arguments are two countries I select to compare and the variable I want to compare.
 compare_plot <- function (country1, country2, indicator) {
-  # country1 <- 'India'
-  # country2 <- 'Japan'
-  # indicator <-  'food_supply'
-  # indicator <- as.character(indicator)
+  
+  #Use if - else to do with different variables
  if(indicator == 'food_supply') {
+   
+   #filter() allows you to subset observations based on their values. 
+   #The first argument is the name of the data frame. 
+   #The second and subsequent arguments are the expressions that filter the data frame. 
+   #we set two subgroups by the country
+   
   tidy_country1 <- filter(tidy_data, country == country1)
   tidy_country2 <- filter(tidy_data, country == country2)
+  
+  #convert the year to numeric so that it could be continuous 
   tidy_country1$year <- as.numeric(tidy_country1$year)
   tidy_country2$year <- as.numeric(tidy_country2$year)
+  
+  #ggplot the graph between the food_supply and year
   ggplot()+
     geom_path(data = tidy_country1, aes(x = year, y = food_supply), group = 1, color = "purple", lwd = 1)+
     geom_path(data = tidy_country2, aes(x = year, y = food_supply), group = 1, color = "orange", lwd =1)
@@ -38,3 +50,7 @@ compare_plot <- function (country1, country2, indicator) {
 } else stop('warning: Check the indicator!!!!')
 }
 
+#input the variable and use the function to get the plot
+compare_plot("Solomon Islands", "Thailand",'sugar')
+compare_plot("Solomon Islands", "Thailand",'food_supply')
+compare_plot("Solomon Islands", "Thailand",'percentage')
--- wrangling_code.R
+++ wrangling_code.R
@@ -1,21 +1,29 @@
+#Load the packages: tidyr, dplyr
 library(readxl)
 library(tidyr)
 library(dplyr)
 
+#Read the excel file and get the dataset of food supply and sugar
 food_supply <- read_excel("food_supply_kilocalories_per_person_and_day.xlsx")
 sugar <- read_excel("sugar_per_person_g_per_day.xlsx")
 
-#pivot_longer
+#To tidy a dataset, pivot the offending columns into a new pair of variables
 tidy_food_supply <- pivot_longer(food_supply, 2:54, names_to = "year", values_to = "food_supply")
 tidy_sugar <- pivot_longer(sugar, '1960':'2012', names_to = "year", values_to = "sugar")
+
+#To combine the tidied versions of tidy_sugar and tidy_food_supply into a single tibble, 
+#we need to use left_join(), which you’ll learn about in relational data.
 tidy_data <- left_join(tidy_food_supply, tidy_sugar)
 str(tidy_data)
+
+#Drop tibble's rows containing missing values
 tidy_data <- drop_na(tidy_data)
+
+#Mutate adds new variables and preserves existing
+#the new variable is percentage which is sugar/food_supply
 tidy_data <- mutate(tidy_data, 
                     percentage = sugar/food_supply)
 
-tidy_Solomon_Islands <- filter(tidy_data, country == "Solomon Islands")
-tidy_Thailand <- filter(tidy_data, country == "Thailand")
 
 #gather
 #tidy_food_supply <- food_supply %>%
